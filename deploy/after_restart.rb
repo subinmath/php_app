70b7329ac67920c045a33fa0a2036ddf6f60d5c9
subinmath <subin.mathew@crichq.com>
2016-08-24 10:42:46 +0530

    test

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,1 +1,7 @@
-system "rm -r", "/srv/www/php_test/MyTestFile"
+
+  code <<-EOH
+  curl -X POST \
+  -H 'Content-type: application/json' \
+  --data '{"text": "This is posted to <#ops_test> and comes from *test-bot*.", "channel": "#ops_test", "username": "test-bot", "icon_emoji": ":monkey_face:"}' \
+  https://hooks.slack.com/services/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
+  EOH
--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,1 +1,12 @@
-exec "mkdir","/srv/www/php_test/test.$(date "+%Y.%m.%d-%H.%M.%S")"
+bash 'install_something' do
+  user 'root'
+  cwd '/tmp'
+  code <<-EOH
+  wget http://sdlc-esd.oracle.com/ESD6/JSCDL/jdk/8u101-b13/jre-8u101-linux-x64.tar.gz?GroupName=JSC&FilePath=/ESD6/JSCDL/jdk/8u101-b13/jre-8u101-linux-x64.tar.gz&BHost=javadl.sun.  com&File=jre-8u101-linux-x64.tar.gz&AuthParam=1472016267_d762eedb02d4084047ee76ab3dcccc71&ext=.gz
+  tar -zxf *.tar.gz
+  #cd tarball
+  #./configure
+  #make
+  #make install
+  EOH
+end
