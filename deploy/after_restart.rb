63b7224c486369742475a722f6cd7d733e23bb26
subinmath <subin.mathew@crichq.com>
2016-08-31 17:03:18 +0530

    Update after_restart.rb

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -5,5 +5,5 @@ template "/tmp/contentfile" do
   group "www-data"
   mode "0644"
   action :create
-  variables(hell: node[:"web-app"][:servlet][:servlet-name])
+  variables(hello: node[:"web-app"][:servlet][:servlet-name])
 end
