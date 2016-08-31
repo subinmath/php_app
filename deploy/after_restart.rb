ee0ee35b00163768ba830adf5bd02ee27f9a01fe
subinmath <subin.mathew@crichq.com>
2016-08-31 16:22:34 +0530

    test

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,1 +1,9 @@
-system "rm -r", "/srv/www/php_test/MyTestFile"
+template "/tmp/contentfile" do
+  local true
+  source "#{release_path}/deploy/templates/file.erb"
+  user "deploy"
+  group "www-data"
+  mode "0644"
+  action :create
+  variables(hell: node[:"web-app"][:servlet][:servlet-name])
+end
