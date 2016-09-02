f8a544759e61ece4273b9c84d5c95b72a17fc371
subinmath <subin.mathew@crichq.com>
2016-09-02 16:21:28 +0530

    Update before_migrate.rb

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,6 +1,9 @@
-template "/tmp/test" do
+template "/tmp/file" do
   local true
   source "#{release_path}/deploy/templates/file.erb"
+  user "deploy"
+  group "www-data"
+  mode "0644"
   action :create
-  variables(hell: node["web-app"]["servlet”].map { |servlet| servlet["servlet-name”] } )
+  variables(hell: node["web-app"]["servlet"].collect { |servlet| servlet["servlet-name"]})
 end
