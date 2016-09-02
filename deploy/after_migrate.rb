f63838e78e333aa38bd20d8f24f6a7f7583b6893
subinmath <subin.mathew@crichq.com>
2016-09-02 11:48:55 +0530

    Update after_migrate.rb

--- deploy/after_migrate.rb
+++ deploy/after_migrate.rb
@@ -1,9 +1,6 @@
-template "/tmp/contentfile" do
+template "/tmp/test" do
   local true
   source "#{release_path}/deploy/templates/file.erb"
-  user "deploy"
-  group "www-data"
-  mode "0644"
   action :create
   variables(hell: node["web-app"]["servlet"]["servlet-name"].each do |name|)
  end
